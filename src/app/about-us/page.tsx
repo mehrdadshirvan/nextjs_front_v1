a2340c9e79e9a2685699ab87b29fb397c82bd7d1
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2024-02-25 17:24:17 +0330

    metadata

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,8 @@
-export default function AboutUS(){
+export const metadata = {
+    title: "about us",
+    description: "about us description"
+};
+export default function AboutUS() {
     return (
         <>
             <h1>About us</h1>
