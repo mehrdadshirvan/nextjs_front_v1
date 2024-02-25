48324a95e232ee23ffae7043816b6b9701dbca1c
Mehrdad Shirvan <mysmartapply.it@gmail.com>
2024-02-25 17:32:11 +0330

    metadata

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,10 +1,28 @@
-export default function ProductSinglePage(
-    {
-        params
-    }: {
-        params : {slug: string}
-    }){
+import {Metadata} from "next";
 
+type Props = {
+    params:{
+        slug:string;
+    }
+}
+
+export const generateMetadata = async ({params}:Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+         setTimeout(()=>{
+             resolve(`iphone ${params.slug}`)
+         },100)
+    })
+    return {
+        title: `${title}`
+    }
+}
+
+// export const generateMetadata = ({params}:Props): Metadata => {
+//     return {
+//         title: `${params.slug}`
+//     }
+// }
+export default function ProductSinglePage({params}: Props){
     return (
         <>
             <h1>ProductSinglePage {params.slug}</h1>
